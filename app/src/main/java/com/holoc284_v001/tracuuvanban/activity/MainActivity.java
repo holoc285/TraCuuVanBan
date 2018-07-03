aefe8bbca374452ceafb6441a3242f4153250769
holoc285 <holoc285@gmail.com>
2018-07-03 22:18:13 +0700

    Update chức năng thống kê theo user

--- app/src/main/java/com/holoc284_v001/tracuuvanban/activity/MainActivity.java
+++ app/src/main/java/com/holoc284_v001/tracuuvanban/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -33,10 +34,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.holoc284_v001.tracuuvanban.R;
 import com.holoc284_v001.tracuuvanban.adapter.ConnectivityReceiver;
 import com.holoc284_v001.tracuuvanban.adapter.MyApplication;
-import com.holoc284_v001.tracuuvanban.R;
 import com.holoc284_v001.tracuuvanban.fragment.FragmentBieuDo;
+import com.holoc284_v001.tracuuvanban.fragment.FragmentBieuDoTong;
 import com.holoc284_v001.tracuuvanban.fragment.FragmentInfo;
 import com.holoc284_v001.tracuuvanban.fragment.FragmentTraCuu;
 import com.holoc284_v001.tracuuvanban.fragment.FragmentVanBanDaXuLy;
@@ -51,23 +53,28 @@ import org.json.JSONObject;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener{
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
     public static String text;
-    SearchView searchView;
-    MenuItem menuItem;
-    TextView txtUser, txtName, txtAddress;
+    private SearchView searchView;
+    private MenuItem menuItem;
+    private TextView txtUser, txtName, txtAddress;
     public static String userName;
     public static int userId;
     public static Context context;
     public static Activity activity;
 
-    Fragment fragment = null;
+    private Fragment fragment = null;
+    private List<User> users;
+    private String tongvb    =   null;
+    private String daxuly    =   null;
+    private String chuaxuly  =   null;
 
-    public String tongvb=null;
-    public String daxuly=null;
-    public String chuaxuly = null;
+    private String tongvbAdmin    =   null;
+    private String daxulyAdmin    =   null;
+    private String chuaxulyAdmin  =   null;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,18 +82,15 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
         setContentView(R.layout.activity_main);
 
         if (isConnected()){
-
             checkConnection();
             context = getApplicationContext();
-
             activity = this;
-
+            getInfoUser();
             getTK();
+            getTkTong();
 
             //Toast.makeText(this, Path.LOCALHOST, Toast.LENGTH_SHORT).show();
-
             loadFragment(new FragmentTraCuu());
-
             drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
             navigationView = (NavigationView) findViewById(R.id.navigationViewTTKH);
             toolbar = (Toolbar) findViewById(R.id.toolBarC);
@@ -103,50 +107,101 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
             BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.myBTN);
 
 
-            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if (users.get(0).getUserName().equals("admin")){
+                getTK();
+                getTkTong();
+                bottomNavigationView.inflateMenu(R.menu.bottom_nav_main_admin);
 
-                    switch (item.getItemId()) {
-                        case R.id.danhSachVB:
-                            getTK();
-                            checkConnection();
-                            fragment = new FragmentTraCuu();
-                            if (menuItem.isVisible()!= true) {
-                                menuItem.setVisible(true);
-                            }
-                            break;
-                        case R.id.traCuu:
-                            checkConnection();
-                            fragment = new FragmentVanBanDaXuLy();
-                            if (menuItem.isVisible()!= true) {
-                                menuItem.setVisible(true);
-                            }
-                            break;
-                        case R.id.bieuDo:
-                            checkConnection();
-                            getTK();
-                            fragment = new FragmentBieuDo();
-                            menuItem.setVisible(false);
-                            break;
-//                    case user:
-//                        fragment = new fragmentUser();
-//                        menuItem.setVisible(false);
-//                        break;
-                        case R.id.info:
-                            checkConnection();
-                            fragment = new FragmentInfo();
-                            menuItem.setVisible(false);
-                            break;
+                bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        switch (item.getItemId()) {
+                            case R.id.danhSachVB:
+                                checkConnection();
+                                getTK();
+                                getTkTong();
+                                fragment = new FragmentTraCuu();
+                                if (menuItem.isVisible()!= true) {
+                                    menuItem.setVisible(true);
+                                }
+                                break;
+                            case R.id.traCuu:
+                                checkConnection();
+                                fragment = new FragmentVanBanDaXuLy();
+                                if (menuItem.isVisible()!= true) {
+                                    menuItem.setVisible(true);
+                                }
+                                break;
+                            case R.id.bieuDoUser:
+                                checkConnection();
+                                getTK();
+                                fragment = new FragmentBieuDo();
+                                menuItem.setVisible(false);
+                                break;
+                            case R.id.bieuDoTong:
+                                checkConnection();
+                                getTkTong();
+                                fragment = new FragmentBieuDoTong();
+                                menuItem.setVisible(false);
+                                break;
+                            case R.id.info:
+                                checkConnection();
+                                fragment = new FragmentInfo();
+                                menuItem.setVisible(false);
+                                break;
+                        }
+                        return loadFragment(fragment);
                     }
-                    return loadFragment(fragment);
-                }
-            });
+                });
+            }else {
+                //User
+                getTK();
+                getTkTong();
+                bottomNavigationView.inflateMenu(R.menu.bottom_nav_main);
+                bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-            getInfoUser();
+                        switch (item.getItemId()) {
+                            case R.id.danhSachVB:
+                                getTK();
+                                getTkTong();
+                                checkConnection();
+                                fragment = new FragmentTraCuu();
+                                if (menuItem.isVisible()!= true) {
+                                    menuItem.setVisible(true);
+                                }
+                                break;
+                            case R.id.traCuu:
+                                checkConnection();
+                                fragment = new FragmentVanBanDaXuLy();
+                                if (menuItem.isVisible()!= true) {
+                                    menuItem.setVisible(true);
+                                }
+                                break;
+                            case R.id.bieuDo:
+                                checkConnection();
+                                getTK();
+                                fragment = new FragmentBieuDo();
+                                menuItem.setVisible(false);
+                                break;
+                            case R.id.info:
+                                checkConnection();
+                                fragment = new FragmentInfo();
+                                menuItem.setVisible(false);
+                                break;
+                        }
+                        return loadFragment(fragment);
+                    }
+                });
+            }
             daXuLy();
             tongVb();
             chuaXuLy();
+            tongVbAdmin();
+            daXuLyAdmin();
+            chuaXuLyAdmin();
 
         }else {
             AlertDialog.Builder dialog = new AlertDialog.Builder(this);
@@ -192,7 +247,7 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
 
         final SQLiteQueryUser sqLiteQueryUser = SQLiteQueryUser.getINSTANCE(this);
 
-        List<User> users = sqLiteQueryUser.getUserID(userName);
+        users = sqLiteQueryUser.getUserID(userName);
         txtUser.setText(users.get(0).getUserName());
         txtName.setText(users.get(0).getName());
         txtAddress.setText(users.get(0).getEmail());
@@ -237,6 +292,13 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1000 && data!=null){
+            loadFragment(new FragmentTraCuu());
+        }
+    }
 
     public boolean loadFragment(Fragment fragment) {
         //switching fragment
@@ -260,10 +322,23 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
         return chuaxuly;
     }
 
-    public void getTK(){
-        String url = "http://"+Path.LOCALHOST+"/api/tracuuvanban/thongke";
-        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+    public String tongVbAdmin(){
+        return tongvbAdmin;
+    }
+    public String daXuLyAdmin(){
+        return daxulyAdmin;
+    }
+    public String chuaXuLyAdmin(){
+        return chuaxulyAdmin;
+    }
 
+    public void getTK(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Vui lòng chờ ...");
+        progressDialog.setCanceledOnTouchOutside(true);
+        progressDialog.show();
+        String url = "http://"+Path.LOCALHOST+"/api/tracuuvanban/thongkeUser/"+users.get(0).getUserId();
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -273,23 +348,9 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
                         tongvb = jsonObject.getString("tongsovb").replace("[", "").replace("]", "");
                         daxuly = jsonObject.getString("daxuly").replace("[", "").replace("]", "");
                         chuaxuly = jsonObject.getString("chuaxuly").replace("[", "").replace("]", "");
-
-                        int tong = Integer.parseInt(tongvb);
-                        int dxl = Integer.parseInt(daxuly);
-                        int cxl = Integer.parseInt(chuaxuly);
-                        float d = (float) dxl / tong * 100;
-                        float c = (float) cxl / tong * 100;
-                        //yvalues.add(new Entry(d,0));
-                        //yvalues.add(new Entry(c,1));
-                        //Toast.makeText(MainActivity.this, d + " " + c, Toast.LENGTH_SHORT).show();
-
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-
-                    //progressDialog.dismiss();
-                }else {
-
                 }
             }
         }, new Response.ErrorListener() {
@@ -297,12 +358,47 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
             public void onErrorResponse(VolleyError error) {
             }
         });
+
+        if (tongvbAdmin!=null && daxulyAdmin!=null && chuaxulyAdmin!=null){
+            progressDialog.dismiss();
+        }
         requestQueue.add(stringRequest);
     }
 
+    public void getTkTong(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Vui lòng chờ ...");
+        progressDialog.setCanceledOnTouchOutside(true);
+        progressDialog.show();
 
+        String url = "http://"+Path.LOCALHOST+"/api/tracuuvanban/thongke";
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response!=null) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        tongvbAdmin = jsonObject.getString("tongsovb").replace("[", "").replace("]", "");
+                        daxulyAdmin = jsonObject.getString("daxuly").replace("[", "").replace("]", "");
+                        chuaxulyAdmin = jsonObject.getString("chuaxuly").replace("[", "").replace("]", "");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        if (tongvbAdmin!=null && daxulyAdmin!=null && chuaxulyAdmin!=null){
+            progressDialog.dismiss();
+        }
+        requestQueue.add(stringRequest);
+    }
 
     // Method to manually check connection status
     private void checkConnection() {
